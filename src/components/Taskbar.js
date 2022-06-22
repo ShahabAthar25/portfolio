d9f8a33956a51d5205192fd87b2f11ee3988a955
ShahabAthar25 <shahabathar25@gmail.com>
2022-06-22 11:58:34 +0500

    Add search field

--- src/components/Taskbar.js
+++ src/components/Taskbar.js
@@ -1,9 +1,14 @@
+import { SearchIcon } from "@heroicons/react/outline";
 import windowsLogo from "../images/windowsLogo.png";
 
 export default function Taskbar() {
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="fixed bottom-0 w-screen bg-[#213540]">
-      <button className="px-2 py-1 cursor-default">
+    <div className="fixed bottom-0 w-screen bg-[#213540] flex">
+      <button className="px-0.5 py-1 cursor-default hover:bg-gray-700 outline-none">
         <img
           src={windowsLogo}
           alt=""
@@ -11,6 +16,19 @@ export default function Taskbar() {
           draggable="false"
         />
       </button>
+      <form className="bg-[#f2f2f2] w-[343px] flex items-center">
+        <button
+          className="bg-transparent border-none outline-none cursor-text"
+          onClick={(e) => handleOnSubmit(e)}
+        >
+          <SearchIcon className="h-6 rotate-90 px-2 text-gray-600" />
+        </button>
+        <input
+          type="text"
+          className="bg-transparent border-none w-full h-full outline-none"
+          placeholder="Type here to search"
+        />
+      </form>
     </div>
   );
 }
