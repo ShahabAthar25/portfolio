cdb4534b3e509dca56af87842d06ad60891ab4c1
ShahabAthar25 <shahabathar25@gmail.com>
2022-06-23 14:37:06 +0500

    Add React

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import Taskbar from "./components/taskbar/Taskbar";
-import background from "./images/background.jpg";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div
-      className="w-screen h-screen bg-cover"
-      style={{ backgroundImage: `url(${background})` }}
-    >
-      <Taskbar />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
