c6b0108a1da6289afb8114dacbc4f7175c8a34ba
ShahabAthar25 <shahabathar25@gmail.com>
2022-06-23 21:58:47 +0500

    Add navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,9 +1,36 @@
+import { Link } from "react-router-dom";
 import logo from "../images/logo.png";
 
 export default function Navbar() {
   return (
-    <div className="flex items-center justify-between px-4">
-      <img src={logo} alt="" className="h-20" />
+    <div className="flex items-center justify-between px-20 py-3">
+      <img src={logo} alt="" className="h-20 cursor-pointer" />
+      <div className="flex items-center space-x-10">
+        <Link
+          to="/projects"
+          className="text-2xl font-marker hover:text-gray-600 transition-all"
+        >
+          Projects
+        </Link>
+        <Link
+          to="/projects"
+          className="text-2xl font-marker hover:text-gray-600 transition-all"
+        >
+          About Me
+        </Link>
+        <Link
+          to="/projects"
+          className="text-2xl font-marker hover:text-gray-600 transition-all"
+        >
+          Web Stack
+        </Link>
+        <Link
+          to="/projects"
+          className="text-2xl font-marker hover:text-gray-600 transition-all"
+        >
+          Contact Me
+        </Link>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+      marker: ["Permanent Marker", "cursive"],
+    },
   },
   plugins: [],
 };
