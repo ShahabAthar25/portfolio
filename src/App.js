ba7620d63f04d95019b44003ff42afdf86e60086
ShahabAthar25 <shahabathar25@gmail.com>
2022-06-26 10:17:08 +0500

    Add react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./component/Navbar";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="bg-[#040B10] w-screen h-screen text-white overflow-x-hidden">
+    <div>
       <BrowserRouter>
-        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
